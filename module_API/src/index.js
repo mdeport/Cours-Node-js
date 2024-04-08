b96aa83cce4643cacdd62969ec5ad29b42497ea4
mdeport <mateo.deport@ecole-isitech.fr>
2024-04-08 17:02:47 +0200

    deuxieme commit lundi apres midi pour avoir toutes info sur voiture

--- module_API/src/index.js
+++ module_API/src/index.js
@@ -1,11 +1,21 @@
 import express from "express";
+import carsRouter from "./routes/cars.js";
 
 const app = express();
 
 const Port = process.env.PORT || 3001;
 
+//middleware
+//middleware qui se charge de rediriger les req
+//qui concernents les voitures vers le router des voitures
+//http://localhost:3001/cars
+
+app.use(express.json());
+
+app.use("/cars", carsRouter);
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.json({ message: "Hello World!" });
 });
 
 app.listen(Port, () => {
